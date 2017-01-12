b8ac7a59d820d683298beddfbe07dc02f2f76948
steve111MV <unlivregratuit@gmail.com>
2017-01-12 16:30:12 +0100

    refactor:  Adit MdbMovie object to handle rating

--- app/src/main/java/cg/stevendende/popularmovies/model/MdbMovie.java
+++ app/src/main/java/cg/stevendende/popularmovies/model/MdbMovie.java
@@ -4,10 +4,13 @@
 
 package cg.stevendende.popularmovies.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Represents the Model of one Movie from THEMOVIEDB API
  */
-public class MdbMovie {
+public class MdbMovie implements Parcelable{
 
     private String titre;
     private String imageUrl;
@@ -20,11 +23,57 @@ public class MdbMovie {
     private String originalLanguage;
     private String releaseDate;
     private String genres;
+    private float rating;
     private boolean videoEnabled;
     private long id;
 
 
     public MdbMovie() {
+    }
+
+    protected MdbMovie(Parcel in) {
+        titre = in.readString();
+        imageUrl = in.readString();
+        imagePath = in.readString();
+        posterUrl = in.readString();
+        posterPath = in.readString();
+        adult = in.readByte() != 0;
+        decription = in.readString();
+        originalTitle = in.readString();
+        originalLanguage = in.readString();
+        releaseDate = in.readString();
+        genres = in.readString();
+        rating = in.readFloat();
+        videoEnabled = in.readByte() != 0;
+        id = in.readLong();
+    }
+
+    public static final Creator<MdbMovie> CREATOR = new Creator<MdbMovie>() {
+        @Override
+        public MdbMovie createFromParcel(Parcel in) {
+            return new MdbMovie(in);
+        }
+
+        @Override
+        public MdbMovie[] newArray(int size) {
+            return new MdbMovie[size];
+        }
+    };
+
+    public String getGenres() {
+        return genres;
+    }
+
+    public void setGenres(String genres) {
+        this.genres = genres;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    public void setRating(float rating) {
+        this.rating = rating;
     }
 
     public String getTitre() {
@@ -121,6 +170,48 @@ public class MdbMovie {
 
     public void setId(long id) {
         this.id = id;
+    }
+
+
+    /**
+     * Describe the kinds of special objects contained in this Parcelable
+     * instance's marshaled representation. For example, if the object will
+     * include a file descriptor in the output of {@link #writeToParcel(Parcel, int)},
+     * the return value of this method must include the
+     * {@link #CONTENTS_FILE_DESCRIPTOR} bit.
+     *
+     * @return a bitmask indicating the set of special object types marshaled
+     * by this Parcelable object instance.
+     * @see #CONTENTS_FILE_DESCRIPTOR
+     */
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    /**
+     * Flatten this object in to a Parcel.
+     *
+     * @param dest  The Parcel in which the object should be written.
+     * @param flags Additional flags about how the object should be written.
+     *              May be 0 or {@link #PARCELABLE_WRITE_RETURN_VALUE}.
+     */
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(titre);
+        dest.writeString(imageUrl);
+        dest.writeString(imagePath);
+        dest.writeString(posterUrl);
+        dest.writeString(posterPath);
+        dest.writeByte((byte) (adult ? 1 : 0));
+        dest.writeString(decription);
+        dest.writeString(originalTitle);
+        dest.writeString(originalLanguage);
+        dest.writeString(releaseDate);
+        dest.writeString(genres);
+        dest.writeFloat(rating);
+        dest.writeByte((byte) (videoEnabled ? 1 : 0));
+        dest.writeLong(id);
     }
 }
 
