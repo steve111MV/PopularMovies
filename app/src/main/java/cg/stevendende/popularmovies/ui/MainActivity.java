267b462c998f04a14a59d46a84542e0a8b7258e4
steve111MV <unlivregratuit@gmail.com>
2017-01-12 17:59:16 +0100

    refactor: User can access details
    Implement a onItemClickListener that transitions to the movie details
    - MainActivity toolbar moved to MainFragment

--- app/src/main/java/cg/stevendende/popularmovies/ui/MainActivity.java
+++ app/src/main/java/cg/stevendende/popularmovies/ui/MainActivity.java
@@ -5,31 +5,63 @@
 package cg.stevendende.popularmovies.ui;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import cg.stevendende.popularmovies.R;
+import cg.stevendende.popularmovies.model.MdbMovie;
 
 /**
  * This Activity is the main entrance of The Application.
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MainActivityFragment.CallbackInterface {
+
+    public static final String EXTRA_MOVIE = "cg.stevendende.extra.movie";
+    private static final String TAG_FRAGMENT_MAIN = "tag.main.fragment";
+    private static final String TAG_FRAGMENT_DETAILS = "tag.details.fragment";
+    private String mFragmentTag = TAG_FRAGMENT_MAIN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        getSupportActionBar().setLogo(R.mipmap.ic_launcher);
-        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        setContentView(R.layout.activity_main);
+
+        if (getSupportFragmentManager().findFragmentByTag(mFragmentTag) == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, new MainActivityFragment(), mFragmentTag)
+                    .commit();
+        }
+    }
+
+    @Override
+    public void onMovieItemClieckListener(MdbMovie movie) {
+
+        Bundle data = new Bundle();
+        data.putParcelable(EXTRA_MOVIE, movie);
+
+        DetailsFragment detailsFragment = new DetailsFragment();
+        detailsFragment.setArguments(data);
 
         getSupportFragmentManager()
                 .beginTransaction()
-                .replace(R.id.fragment_container, new MainActivityFragment())
+                .replace(R.id.fragment_container, detailsFragment, mFragmentTag)
+                .setCustomAnimations(android.R.anim.slide_in_left, android.R.anim.slide_out_right)
+                .addToBackStack(null)
                 .commit();
 
+        mFragmentTag = TAG_FRAGMENT_DETAILS;
     }
 
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+    }
 }
--- app/src/main/java/cg/stevendende/popularmovies/ui/MainActivityFragment.java
+++ app/src/main/java/cg/stevendende/popularmovies/ui/MainActivityFragment.java
@@ -4,18 +4,23 @@
 
 package cg.stevendende.popularmovies.ui;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.Toast;
 
@@ -25,15 +30,19 @@ import cg.stevendende.popularmovies.MdbMovieListAdapter;
 import cg.stevendende.popularmovies.R;
 import cg.stevendende.popularmovies.ServerAsyncTask;
 import cg.stevendende.popularmovies.Tools;
+import cg.stevendende.popularmovies.model.MdbMovie;
 import cg.stevendende.popularmovies.model.MdbMovieList;
 
 /**
  * Displays the list of popular Movies in the MainActivity
  */
-public class MainActivityFragment extends Fragment {
+public class MainActivityFragment extends Fragment implements AdapterView.OnItemClickListener {
 
     private GridView mGridView;
+    private Toolbar toolbar;
     private MdbMovieListAdapter mListAdapter;
+
+    private CallbackInterface mCallback;
 
     public MainActivityFragment() {
     }
@@ -41,7 +50,7 @@ public class MainActivityFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setRetainInstance(true);
         setHasOptionsMenu(true);
     }
 
@@ -50,37 +59,42 @@ public class MainActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
+        toolbar = (Toolbar) rootView.findViewById(R.id.toolbar);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setLogo(R.mipmap.ic_launcher);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayUseLogoEnabled(true);
+
         mGridView = (GridView) rootView.findViewById(R.id.gridView);
-        int mVerticalSpacing = 1;
-        int mHorinzontalSpacing = 1;
+
         int mNumberOfCols = 2;
 
+        //Dyna
         if (Tools.isTabletScreen(getActivity())) {
             if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
                 mNumberOfCols = 3;
             } else {
                 mNumberOfCols = 4;
             }
-            mVerticalSpacing = 2;
-            mHorinzontalSpacing = 2;
         } else {
-            /** */
             if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
                 mNumberOfCols = 2;
             } else {
                 mNumberOfCols = 3;
             }
-            mVerticalSpacing = 2;
-            mHorinzontalSpacing = 2;
         }
 
         mGridView.setNumColumns(mNumberOfCols);
-        mGridView.setVerticalSpacing(mVerticalSpacing);
-        mGridView.setHorizontalSpacing(mHorinzontalSpacing);
+        if (mListAdapter == null) {
+            mListAdapter = new MdbMovieListAdapter(new MdbMovieList(), getActivity());
+        } else {
+            //Avoid Glide "java.lang.IllegalArgumentException: You cannot start a load for a destroyed activity"
+            mListAdapter.setContext(getActivity());
+        }
 
-        mListAdapter = new MdbMovieListAdapter(new MdbMovieList(), getActivity());
         mGridView.setAdapter(mListAdapter);
 
+        mGridView.setOnItemClickListener(this);
         return rootView;
     }
 
@@ -98,6 +112,8 @@ public class MainActivityFragment extends Fragment {
             e.printStackTrace();
             Toast.makeText(getActivity(), "No internet access, check your internet settings", Toast.LENGTH_SHORT).show();
         }
+
+        Log.i("fragment life cycle", "onStart()");
     }
 
     /**
@@ -117,7 +133,7 @@ public class MainActivityFragment extends Fragment {
                     mListAdapter.notifyDataSetChanged();
 
                 } else {
-                    Toast.makeText(getActivity(), "Connetion error, try again !", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getActivity(), "Connetion error, try again !", Toast.LENGTH_LONG).show();
                 }
             }
         }.execute(mSortBy);
@@ -139,5 +155,35 @@ public class MainActivityFragment extends Fragment {
         }
 
         return true;
+    }
+
+    /**
+     * Callback method to be invoked when an item in this AdapterView has
+     * been clicked.
+     * <p>
+     * Implementers can call getItemAtPosition(position) if they need
+     * to access the data associated with the selected item.
+     *
+     * @param parent   The AdapterView where the click happened.
+     * @param view     The view within the AdapterView that was clicked (this
+     *                 will be a view provided by the adapter)
+     * @param position The position of the view in the adapter.
+     * @param id       The row id of the item that was clicked.
+     */
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        if (mCallback != null) {
+            mCallback.onMovieItemClieckListener((MdbMovie) mListAdapter.getItem(position));
+        }
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        this.mCallback = (CallbackInterface) activity;
+    }
+
+    public interface CallbackInterface {
+        void onMovieItemClieckListener(MdbMovie movie);
     }
 }
